ae9cf40834b60a5fc3dd4938790c3ab4e1224d43
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-12 14:06:04 -0700

    miramos los tipos de datos

--- ejemplos.js
+++ ejemplos.js
@@ -1,1 +1,16 @@
+/*-
+tipos de datos primitivos
+string
+number
+boolean
+null
+undefined
+NAN
+-Tipos de Datos Compluestos
+object = {}
+array = []
+funcion(){}
+Class{}
+etc.
+*/
 console.log("hola mundo")
