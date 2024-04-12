98c156f3dfbcb6494cca5f4f565816c58ace7dcd
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-12 15:36:47 -0700

    declaramos strings para imprimirlas con el console.log

--- ejemplos.js
+++ ejemplos.js
@@ -1,16 +1,37 @@
 /*-
-tipos de datos primitivos
+tipos de datos primitivos(se accede directamente al valor)
 string
 number
 boolean
-null
+null  
 undefined
 NAN
--Tipos de Datos Compluestos
+-Tipos de Datos Compluestos(se accede a la referencia del valor)
 object = {}
 array = []
 funcion(){}
 Class{}
 etc.
 */
+
+//console.log permite imprimir codigo en la consola
 console.log("hola mundo")
+
+// var es para variables globales(mala practica)y let se usa en el ambito de bloque
+ 
+/* podemos usar constantes para declarar objetos o arreglos, y tambien datos primitivos, pero estos
+no pueden cambiar su valor por ser constante
+*/
+
+//cadenas de texto, strings
+
+let nombre = "isaias"
+let apellido = "Salgado"
+let saludo = "hola mundo"
+
+console.log(nombre, apellido, saludo)
+console.log(
+    nombre.length,
+    apellido.length,
+    saludo.length
+)
