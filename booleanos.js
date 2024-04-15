df32bd1419831a332cd9de9f37a0973ed3599fb2
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-15 15:02:20 -0700

    creamos booleanos.js para crear ejemplos de estas variables

---
+++ booleanos.js
@@ -0,0 +1,8 @@
+//aqui podemos ver los dos tipos de datos booleanos
+let verdadero = true
+let falso = false
+console.log(verdadero, falso)
+/*hay valores que tieden a verdadero como: objetos vacios, un arreglo vacio, condicional true, una fecha
+un numero, un numero negativo, etc
+o que tienden a falso como: null, undefined, el numero 0, NaN, cadena de texto vacia, etc
+*/
