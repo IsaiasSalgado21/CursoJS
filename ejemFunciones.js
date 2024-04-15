96013b1ec8bc0dcaf2bf97873b16ecb2a9640857
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-15 16:00:11 -0700

    creamos un archivo para usar funciones

---
+++ ejemFunciones.js
@@ -0,0 +1,12 @@
+//creamos una funcion
+function nombreDeMiFuncion(){
+    console.log("hola, soy una funcion")
+}
+//aqui le pasamos los parametros de nombre y edad a nuestra funcion
+function saludo(nombre, edad){
+    console.log(`Hola mi nombre es ${nombre} y tengo ${edad} años`)
+}
+
+//aqui mandamos a llamar nuestra funcion
+nombreDeMiFuncion()
+saludo("sanji", 2)
