8c7dbb02ea807d14544899fc5f37ae58afbbcf5c
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-17 15:00:33 -0700

    usamos algunas funciones y ahora creamos el archivo arraysEjem.js

--- ejemFunciones.js
+++ ejemFunciones.js
@@ -2,11 +2,20 @@
 function nombreDeMiFuncion(){
     console.log("hola, soy una funcion")
 }
-//aqui le pasamos los parametros de nombre y edad a nuestra funcion
-function saludo(nombre, edad){
+//aqui le pasamos los parametros que resive nuestra funcion1 de nombre y edad a nuestra funcion
+function saludo(nombre ="Desconocido", edad = 0){//aqui definimos los valores de nuestros parametros
     console.log(`Hola mi nombre es ${nombre} y tengo ${edad} años`)
 }
 
 //aqui mandamos a llamar nuestra funcion
 nombreDeMiFuncion()
 saludo("sanji", 2)
+// al no definir los valores de mis parametros estos se muestran como undefined (linea de codigo no.6)
+saludo()
+
+//para declarar una funcion expresada se hace de la siguiente manera:
+//es una buena practica declarar las funciones 
+const funcionExpresada = function(){
+    console.log("soy una funcion expresada")
+}
+funcionExpresada()
