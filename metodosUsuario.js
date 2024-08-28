5c52260292bbedf1e91c7b85875c5025db51011a
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-08-28 16:00:21 -0700

    metodos para interactuar con el usuario con con confirmaciones avisos y alertas

---
+++ metodosUsuario.js
@@ -0,0 +1,12 @@
+console.log(window);
+/*alert("Hola esto es una alerta");
+confirm("Hola esto es una confirmacion");
+prompt("Hola esto es un prompt y le permite al usuario ingresar un valor");*/
+
+let aleta = alert("Hola esto es una alerta");
+let confirmacion = confirm("Hola esto es una confirmacion");
+let aviso = prompt("Hola esto es un aviso y permite al usuario ingresar un valor");
+
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
